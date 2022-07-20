165e0daa5aaf174c6f29b0c0ce4ffdab25d40cee
yashpal21 <yash.pal@altudo.co>
2022-07-20 23:57:16 +0530

    call

---
+++ context-API/src/App.js
@@ -0,0 +1,13 @@
+import CrtCard from './Card.js';
+import './App.css';
+import './css/Card.css';
+
+function App() {
+  return (
+    <div className="App">
+       <CrtCard />
+    </div>
+  );
+}
+
+export default App;
