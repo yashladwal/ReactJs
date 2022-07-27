30d481907766fe658b41ea6b4501d73b235a1807
yashpal21 <yash.pal@altudo.co>
2022-07-27 16:18:04 +0530

    Context API Files Create

--- context-API/src/App.js
+++ context-API/src/App.js
@@ -1,13 +1,23 @@
-import CrtCard from './Card.js';
-import './App.css';
-import './css/Card.css';
+ import React, {createContext } from 'react';
+ import ComA from './PageA';
 
-function App() {
+ const MyName = createContext();
+ const CrtTech = createContext();
+ const UseCon = createContext();
+
+ const App =() => {
   return (
     <div className="App">
-       <CrtCard />
+      <MyName.Provider value={"Yash"}>
+        <CrtTech.Provider value ={"Context API (With Consumer)"}> 
+        <UseCon.Provider value= {"Use Context"}>
+          <ComA />
+          </UseCon.Provider> 
+          </CrtTech.Provider>
+       </MyName.Provider>
     </div>
   );
-}
+};
 
 export default App;
+export { MyName, CrtTech, UseCon };
